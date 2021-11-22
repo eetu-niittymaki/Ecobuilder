d0252a621995b40a7ba4531645af310790164b32
eetu-niittymaki <eetu945@gmail.com>
2021-11-22 20:45:11 +0200

    Improve Fenster.js solution display

---
+++ react/src/Components/Modal.js
@@ -0,0 +1,45 @@
+import * as React from 'react'
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function Modal (props) {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    return (
+        <div style={{ marginBottom: "2%"}}>
+            <Button variant="outlined" onClick={handleClickOpen}>
+                {props.title}
+            </Button>
+            <Dialog
+                open={open}
+                TransitionComponent={Transition}
+                onClose={handleClose}
+                aria-describedby="alert-dialog-slide-description"
+            >
+                <DialogTitle>{props.title}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-slide-description">
+                        {props.contents}
+                    </DialogContentText>
+                </DialogContent>
+            </Dialog>
+      </div>
+    )
+}
--- react/src/Pages/Technical/Fenster.js
+++ react/src/Pages/Technical/Fenster.js
@@ -1,31 +1,21 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
 import Header from "../../Components/Header.js"
+import Modal from "../../Components/Modal.js"
 import "./TechnicalPages.css"
 
+const example = [
+    { title: "U-Wert-/g-Wert-Optimierung", contents: "Ipsum ipsum eos alias reiciendis ex."},
+    { title: "Optimierung das Rahmenanteils", contents: "Reiciendis provident eum laudantium non in odio non."},
+    { title: "Optimierung der Fenstergröße und Ausrichtung", contents: "Placeat porro illum et totam earum voluptas."},
+]
 
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
 });
 
 export default function Fenster() {
-    const [open, setOpen] = React.useState(false);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    }
-
-    const handleClose = () => {
-        setOpen(false);
-    }
-
     return (
         <div className="container">
             <Header />
@@ -40,78 +30,13 @@ export default function Fenster() {
                     width: "20%",
                 }}>
                 </div>
-                <div>
                     <div style={{ marginBottom: "10%", marginLeft: "10%", fontSize: "120%" }}>
                         <img src="../fenster/fenster_icon.png" style={{ marginTop: "5%" }} />
                         <div style={{ marginTop: "10%", textAlign: "center" }}>
-                                <div style={{ marginBottom: "2%"}}> 
-                                    <Button variant="outlined" onClick={handleClickOpen}>
-                                        U-Wert-/g-Wert-Optimierung
-                                    </Button>
-                                    <Dialog
-                                        open={open}
-                                        TransitionComponent={Transition}
-                                        keepMounted
-                                        onClose={handleClose}
-                                        aria-describedby="alert-dialog-slide-description"
-                                    >
-                                        <DialogTitle>{"U-Wert-/g-Wert-Optimierung"}</DialogTitle>
-                                        <DialogContent>
-                                            <DialogContentText id="alert-dialog-slide-description">
-                                                Ipsum ipsum eos alias reiciendis ex. 
-                                                Reiciendis provident eum laudantium non in odio non. 
-                                                Placeat porro illum et totam earum voluptas.
-                                            </DialogContentText>
-                                        </DialogContent>
-
-                                    </Dialog>
-                                </div>
-
-                                <div style={{ marginBottom: "2%"}}>
-                                    <Button variant="outlined" onClick={handleClickOpen}>
-                                        Optimierung das Rahmenanteils
-                                    </Button>
-                                    <Dialog
-                                        open={open}
-                                        TransitionComponent={Transition}
-                                        keepMounted
-                                        onClose={handleClose}
-                                        aria-describedby="alert-dialog-slide-description"
-                                    >
-                                        <DialogTitle>{"Optimierung das Rahmenanteils"}</DialogTitle>
-                                        <DialogContent>
-                                            <DialogContentText id="alert-dialog-slide-description">
-                                                Ipsum ipsum eos alias reiciendis ex. 
-                                                Reiciendis provident eum laudantium non in odio non. 
-                                                Placeat porro illum et totam earum voluptas.
-                                            </DialogContentText>
-                                        </DialogContent>
-                                    </Dialog>
-                                </div>
-
-                                <div style={{ marginBottom: "2%"}}>
-                                    <Button variant="outlined" onClick={handleClickOpen}>
-                                        Optimierung der Fenstergröße und Ausrichtung
-                                    </Button>
-                                    <Dialog
-                                        open={open}
-                                        TransitionComponent={Transition}
-                                        keepMounted
-                                        onClose={handleClose}
-                                        aria-describedby="alert-dialog-slide-description"
-                                    >
-                                        <DialogTitle>{"Optimierung der Fenstergröße und Ausrichtung"}</DialogTitle>
-                                        <DialogContent>
-                                            <DialogContentText id="alert-dialog-slide-description">
-                                                Ipsum ipsum eos alias reiciendis ex. 
-                                                Reiciendis provident eum laudantium non in odio non. 
-                                                Placeat porro illum et totam earum voluptas.
-                                            </DialogContentText>
-                                        </DialogContent>
-                                    </Dialog>
-                                </div>
+                            {example.map((props) => 
+                               <Modal {...props} key={props.title} />
+                            )}   
                         </div>
-                    </div>
                     <div style={{ marginTop: "17%" }}>
                         <img src="../fenster/fenster_footer.png" width="auto" height="auto" />
                     </div>
