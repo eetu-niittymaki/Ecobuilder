629e47c1b346cdb7234cc309faa27ef480a21618
eetu-niittymaki <eetu945@gmail.com>
2021-11-23 15:23:32 +0200

    Change navbar to include all subpages

--- react/src/Components/NavigationMenu.js
+++ react/src/Components/NavigationMenu.js
@@ -3,15 +3,37 @@ import Box from '@mui/material/Box';
 import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import Button from '@mui/material/Button';
 import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
+import { makeStyles } from '@material-ui/core/styles';
+import Collapse from '@material-ui/core/Collapse';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import StarBorder from '@material-ui/icons/StarBorder';
 import { Link } from 'react-router-dom'
 
-export default function SwipeableTemporaryDrawer() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
+  },
+}));
+
+export default function NavigationMenu() {
+  const classes = useStyles();
+  const [openTech, setOpenTech] = React.useState(true);
+  const [openPlan, setOpenPlan] = React.useState(true);
+  const [openOrg, setOpenOrg] = React.useState(true);
   const [state, setState] = React.useState({
     top: false,
     bottom: false,
@@ -31,34 +53,79 @@ export default function SwipeableTemporaryDrawer() {
     setState({ ...state, [anchor]: open });
   };
 
+  const handleClick = (text) => {
+    switch(text) {
+      case "Tech":
+        setOpenTech(!openTech)
+        break
+      case "Plan":
+        setOpenPlan(!openPlan)
+        break
+      case "Org":
+        setOpenOrg(!openOrg)
+        break
+    }
+  }
+
+
   const list = (anchor) => (
     <Box
       sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
       role="presentation"
-      onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
-      <List>
-        {['Technical Solutions', 'Planning Solutions', 'Organisational Solutions'].map((text, index) => (
-          <Link to={`/${text.toLowerCase().split(" ", 1)}`} style={{textDecoration: "none", color: "#2F5A91"}}><ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem></Link>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['Lorem', 'Ipsum', 'Dolor Dei'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
+      {/*TODO: Can different pages be mapped from an array instead of hardcoding them? */}
+      {/*Technical pages*/}
+      <ListItem button onClick={() => handleClick("Tech")}>
+        <Link to="/technical" style={{textDecoration: "none", color: "#2F5A91"}}><ListItemText primary="Technical Solutions"/></Link>
+        {!openTech ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={!openTech} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          {['Balkon', 'Betrieb', 'Brandschutz', 'Dach',  'Fenster',  'Fundament', 'Wand'].map((text, index) => (
+            <Link to={`/technical/${text.toLowerCase()}`} 
+                  style={{textDecoration: "none", color: "#767070"}}>
+                    <ListItem button key={text} className={classes.nested}>
+                      <ListItemText primary={text} />
+                    </ListItem>
+            </Link>
+          ))}
+        </List>
+      </Collapse>
+      {/*Planning pages*/}
+      <ListItem button onClick={() => handleClick("Plan")}>
+        <Link to="/planning" style={{textDecoration: "none", color: "#2F5A91"}}><ListItemText primary="Planning Solutions" /></Link>
+        {!openPlan ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={!openPlan} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          {['Gesatsystem', 'Grundriss', 'Heizung', 'Kühlung', 'Lüftung'].map((text, index) => (
+            <Link to={`/planning/${text.toLowerCase().replace("ü", "u")}`}
+                  style={{textDecoration: "none", color: "#767070"}}>
+              <ListItem button className={classes.nested}>
+                <ListItemText primary={text} />
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+      </Collapse>
+      {/*Organisational pages*/}
+      <ListItem button onClick={() => handleClick("Org")}>
+        <Link to="/organisational" style={{textDecoration: "none", color: "#2F5A91"}}><ListItemText primary="Organisational Solutions" /></Link>
+        {!openOrg ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={!openOrg} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          {['Ausschbreibung', 'Marktanalyse', 'Qualitätsmanagement', 'Vorfertigung', 'Zeitmanagement'].map((text, index) => (
+            <Link to={`/organisational/${text.toLowerCase().replace("ä", "a")}`}
+                  style={{textDecoration: "none", color: "#767070"}}>
+              <ListItem button className={classes.nested}>
+                <ListItemText primary={text} />
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+      </Collapse>
     </Box>
   )
 
