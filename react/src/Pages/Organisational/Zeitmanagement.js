65f7800b1fe813dff2f5919c35401b3c6a496a27
aikasjani <jani.aikas@gmail.com>
2021-11-30 18:16:27 +0200

    Add organisational subpages.

--- react/src/Pages/Organisational/Zeitmanagement.js
+++ react/src/Pages/Organisational/Zeitmanagement.js
@@ -1,9 +1,55 @@
-import React from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Header from "../../Components/Header.js";
+import Button from "@mui/material/Button";
+import "./OrganisationalPages.css";
+
+const onClick = () => {
+  alert("Nothing here yet :(");
+};
 
 export default function Zeitmanagement() {
-    return (
+  return (
+    <div className="container">
+      <Header />
+      <Box className="pageBox">
+        <div
+          style={{
+            backgroundImage: "url(../orga/zeitmanagement/zeitmanagement.png)",
+          }}
+          className="backgroundImg"
+        ></div>
         <div>
-            Zeitmanagement
+          <img
+            src="../orga/zeitmanagement/zeitmanagement_icon.png"
+            className="icon"
+            alt=""
+          />
+          <div>
+            <h1>Your ideas could stand right here!</h1>
+          </div>
+          <Button
+            variant="outlined"
+            onClick={onClick}
+            style={{
+              position: "relative",
+              width: "30%",
+              height: "10%",
+              left: "35%",
+              marginBottom: "5%",
+            }}
+          >
+            share ideas
+          </Button>
+          <div>
+            <img
+              src="../orga/zeitmanagement/zeitmanagement_footer.png"
+              className="footerImg"
+              alt=""
+            />
+          </div>
         </div>
-    )
+      </Box>
+    </div>
+  );
 }
