22619e1ec6f8e5a4816a2e3e504252d436cd31bc
eetu-niittymaki <eetu945@gmail.com>
2021-12-04 23:43:20 +0200

    Add better pdf preview thing

--- react/src/Pages/Literature.js
+++ react/src/Pages/Literature.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import Button from '@mui/material/Button'
 import ItemsCarousel from 'react-items-carousel'
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
 import Header from '../Components/Header.js'
 import ArrowCircleLeftTwoToneIcon from '@mui/icons-material/ArrowCircleLeftTwoTone';
 import ArrowCircleRightTwoToneIcon from '@mui/icons-material/ArrowCircleRightTwoTone';
@@ -8,7 +9,7 @@ import "./Literature.css"
 
 const pdfs = [
     {id: 1, title: "Energy", cover: "../pdf/Energy.png", author: "Jack", file: "../pdf/eshop_app000000-2.pdf"},
-    {id: 2, title: "More energy", cover: "../pdf/Energy.png", author: "Bruce", file: "../pdf/eshop_app000000-2.pdf"},
+    {id: 2, title: "More energy", cover: "../pdf/Energy.png", author: "Bruce", file: "../pdf/20210928_Intake_LfU.pdf"},
     {id: 3, title: "Less energy", cover: "../pdf/Energy.png", author: "Dave", file: "../pdf/eshop_app000000-2.pdf"},
     {id: 4, title: "Yes", cover: "../pdf/Energy.png", author: "some guy", file: "../pdf/eshop_app000000-2.pdf"},
 ]
@@ -33,21 +34,28 @@ export default function Literature() {
                 <ItemsCarousel
                     requestToChangeActive={setActiveItemIndex}
                     activeItemIndex={activeItemIndex}
-                    numberOfCards={(windowWidth <= 800) ? 1 : 3}
+                    numberOfCards={(windowWidth <= 1000) ? 1 : 3}
                     gutter={20}
                     leftChevron={<ArrowCircleLeftTwoToneIcon fontSize="large" style={{color: "#2F5A91"}}/>}
                     rightChevron={<ArrowCircleRightTwoToneIcon fontSize="large" style={{color: "#2F5A91"}} />}
                     chevronWidth={chevronWidth}
                 >
                     {pdfs.map((pdf) => 
-                    <div style={{textAlign: "center", marginBottom: "20%"}}>
-                        <img src={pdf.cover} height="600vh" /> 
-                        <h3 style={{textAlign: "center"}}>{pdf.title}</h3>
-                        <h4>Autoren: {pdf.author}</h4>
-                        <Button variant="contained" style={{ marginLeft: "auto", marginRight: "auto", width: "60%"}}>
-                            <a href={pdf.file} download style={{ color: "white", textDecoration: "none" }}>Download</a>
-                        </Button>
-                    </div>
+                        <div style={{textAlign: "center", marginBottom: "20%", width: "100%"}}>
+                            <Document
+                                 file={pdf.file}
+                                 className="pdfPage"
+                            >
+                                <Page pageNumber={1}
+                                      scale={0.7}
+                                />
+                            </Document>
+                            <h3 style={{textAlign: "center"}}>{pdf.title}</h3>
+                            <h4>Autoren: {pdf.author}</h4>
+                            <Button variant="contained" style={{ marginLeft: "auto", marginRight: "auto", width: "60%"}}>
+                                <a href={pdf.file} download style={{ color: "white", textDecoration: "none" }}>Download</a>
+                            </Button>
+                        </div>
                     )}
                 </ItemsCarousel>
             </div>
