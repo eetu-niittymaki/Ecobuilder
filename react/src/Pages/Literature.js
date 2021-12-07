1262de6e9c5286cd4b9524e15ce94070a5aac8bf
eetu-niittymaki <eetu945@gmail.com>
2021-12-07 11:47:47 +0200

    Add new PDF

--- react/src/Pages/Literature.js
+++ react/src/Pages/Literature.js
@@ -8,10 +8,20 @@ import ArrowCircleRightTwoToneIcon from '@mui/icons-material/ArrowCircleRightTwo
 import "./Literature.css"
 
 const pdfs = [
-    {id: 1, title: "Energy", cover: "../pdf/Energy.png", author: "Jack", file: "../pdf/eshop_app000000-2.pdf"},
-    {id: 2, title: "More energy", cover: "../pdf/Energy.png", author: "Bruce", file: "../pdf/20210928_Intake_LfU.pdf"},
-    {id: 3, title: "Less energy", cover: "../pdf/Energy.png", author: "Dave", file: "../pdf/eshop_app000000-2.pdf"},
-    {id: 4, title: "Yes", cover: "../pdf/Energy.png", author: "some guy", file: "../pdf/eshop_app000000-2.pdf"},
+    {   id: 1, 
+        title: `Optimierungsansätze für kostengünstiges und energieeffizientes Bauen (2021)`,
+        author: `LfU, Ökoernergie-Institut Bayern`, 
+        file: "../pdf/eshop_app000000-2.pdf"
+    },
+    {   id: 2, 
+        title: `Modellvorhaben „KliNaWo“
+                Klimagerechter Nachhaltiger Wohnbau (2017, 2019)`, 
+        author: `Martin Ploss, Tobias Hatt, Christina Schneider, 
+                Thomas Rosskopf, Michael BraunEnergieinstitut Vorarlberg, Dornbirn `, 
+        file: "../pdf/20190729_KliNaWo_Monitoringbericht.pdf"
+    },
+
+
 ]
 
 export default function Literature() {
@@ -34,11 +44,12 @@ export default function Literature() {
                 <ItemsCarousel
                     requestToChangeActive={setActiveItemIndex}
                     activeItemIndex={activeItemIndex}
-                    numberOfCards={(windowWidth <= 900) ? 1 : 3 && (windowWidth <= 1300) ? 2 : 3}
+                    numberOfCards={((windowWidth <= 900) ? 1 : 3) && ((windowWidth <= 1300) ? 2 : 3)}
                     gutter={20}
                     leftChevron={<ArrowCircleLeftTwoToneIcon fontSize="large" style={{color: "#2F5A91"}}/>}
                     rightChevron={<ArrowCircleRightTwoToneIcon fontSize="large" style={{color: "#2F5A91"}} />}
                     chevronWidth={chevronWidth}
+                    outsideChevron
                 >
                     {pdfs.map((pdf) => 
                         <div style={{textAlign: "center", marginBottom: "20%", width: "100%"}}>
@@ -50,12 +61,11 @@ export default function Literature() {
                                       height={600}
                                       scale={0.8}
                                       className="pdfPage"
-
                                 />
                                 </div>
                             </Document>
-                            <h3 style={{textAlign: "center"}}>{pdf.title}</h3>
-                            <h4>Autoren: {pdf.author}</h4>
+                            <h4 style={{textAlign: "center"}}>{pdf.title}</h4>
+                            <h5>Autoren: {pdf.author}</h5>
                             <Button variant="contained" style={{ marginLeft: "auto", marginRight: "auto", width: "60%"}}>
                                 <a href={pdf.file} download style={{ color: "white", textDecoration: "none" }}>Download</a>
                             </Button>
