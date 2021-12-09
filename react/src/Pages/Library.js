dd346b2cdd4cb8c35340aa7abce8b6957d79d5de
aikasjani <jani.aikas@gmail.com>
2021-12-09 14:09:58 +0200

    Add functionality to remove items from library.

--- react/src/Pages/Library.js
+++ react/src/Pages/Library.js
@@ -3,26 +3,20 @@ import Box from "@mui/material/Box";
 import Header from "../Components/Header.js";
 import Modal from "../Components/Modal.js";
 import "./Library.css";
-import { Button } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const example = [
   {
-    title: "Definition und Sicherung der Qualitätsanforderungen",
+    title: "Ipsum ipsum eos alias reiciendis ex. 1",
     contents: "Ipsum ipsum eos alias reiciendis ex.",
   },
   {
-    title: "Definition und Sicherung der Qualitätsanforderungen A",
+    title: "Ipsum ipsum eos alias reiciendis ex. 2",
     contents: "Ipsum ipsum eos alias reiciendis ex.",
   },
   {
-    title: "Definition und Sicherung der Qualitätsanforderungen B",
-    contents: "Ipsum ipsum eos alias reiciendis ex.",
-  },
-];
-
-const example2 = [
-  {
-    title: "Definition und Sicherung der Qualitätsanforderungen",
+    title: "Ipsum ipsum eos alias reiciendis ex. 3",
     contents: "Ipsum ipsum eos alias reiciendis ex.",
   },
 ];
@@ -30,7 +24,7 @@ const example2 = [
 export default function Library() {
   const [savedSolutions, setSavedSolutions] = useState([]);
 
-  // key = avain session storagessa, default = avainta ei löydy
+  // key = variable name in session storage, default = use this, if key not found
   const loadStored = (key, defaultValue) => {
     const stored = sessionStorage.getItem(key);
     if (!stored) {
@@ -39,44 +33,67 @@ export default function Library() {
     return JSON.parse(stored);
   };
 
+  const handleDelete = (storedSolution) => {
+    const storedJSON = JSON.parse(
+      sessionStorage.getItem("ecobuilderSavedSolutions")
+    );
+    for (let i = 0; i < storedJSON.length; i++) {
+      if (storedJSON[i].title === storedSolution.title) {
+        storedJSON.splice(i, 1);
+        sessionStorage.setItem(
+          "ecobuilderSavedSolutions",
+          JSON.stringify(storedJSON)
+        );
+        setSavedSolutions(loadStored("ecobuilderSavedSolutions", []));
+      }
+    }
+  };
+
   //Save an array to session storage.
   useEffect(() => {
-    sessionStorage.setItem("solutions", JSON.stringify(example));
+    sessionStorage.setItem("ecobuilderSavedSolutions", JSON.stringify(example));
   }, []);
 
   //Fetch from session storage.
   useEffect(() => {
-    setSavedSolutions(loadStored("solutions", example2));
-    console.log(JSON.stringify(savedSolutions));
+    setSavedSolutions(loadStored("ecobuilderSavedSolutions", []));
   }, []);
 
   return (
     <div className="container">
       <Header />
+      <div
+        className="libraryHeader"
+        style={{
+          backgroundImage: "url(../library/library.png)",
+        }}
+      >
+        <h2>Bibliothek</h2>
+      </div>
       <Box className="pageBox">
-        <div
-          style={{
-            backgroundImage: "url(../library/library.png)",
-          }}
-          className="backgroundImg"
-        ></div>
-        <div style={{ fontSize: "120%" }}>
-          <div className="modalContainer">
-            {savedSolutions.map((savedSolutions) => (
-              <Modal {...savedSolutions} key={savedSolutions.title} />
-            ))}
+        {savedSolutions.length === 0 ? (
+          <h1>You haven't saved anything yet!</h1>
+        ) : (
+          <div style={{ fontSize: "120%" }}>
+            <div className="modalContainer">
+              {savedSolutions.map((savedSolution) => (
+                <div className="libraryContainer" key={savedSolution.title}>
+                  <Modal {...savedSolution} />
+                  <IconButton
+                    aria-label="delete"
+                    size="small"
+                    onClick={() => handleDelete(savedSolution)}
+                  >
+                    <DeleteIcon fontSize="medium" />
+                  </IconButton>
+                </div>
+              ))}
+            </div>
+            <div className="modalContainer">
+              <Button variant="contained">Export all?</Button>
+            </div>
           </div>
-          <div className="modalContainer">
-            <Button variant="contained">Download?</Button>
-          </div>
-          <div style={{ marginTop: "16%" }}>
-            <img
-              src="../organisational_cover.jpg"
-              className="footerImg"
-              alt="library"
-            />
-          </div>
-        </div>
+        )}
       </Box>
     </div>
   );
