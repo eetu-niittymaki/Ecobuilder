b59db95c0e1ec74c125445977bb8f88bf9f97b30
eetu-niittymaki <eetu945@gmail.com>
2021-12-16 16:50:27 +0200

    Change fragebogen

--- react/src/Pages/ShareIdeas.js
+++ react/src/Pages/ShareIdeas.js
@@ -1,33 +1,52 @@
 import React from 'react'
 import Button from '@mui/material/Button'
 import Header from "../Components/Header.js"
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import "./ShareIdeas.css"
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#2f5a91"
+        }
+    }
+})
 
 export default function ShareIdeas() {
     return (
         <div>
             <Header />
-            <div style={{textAlign: "center", 
-                         marginLeft: "auto", marginRight: "auto", 
-                         marginTop: "10%", 
-                         fontFamily: "Lato"}}>
-                <h1>Teilen Sie Ihre Idee!</h1>
-                <h3 style={{width: "40%", marginLeft: "auto", marginRight: "auto"}}>
-                    Sie haben eine Idee oder Vorschlag für kostengünstiges und energieeffizientes Bauen? 
-                    eilen Sie es mit uns, indem Sie den folgenden Fragebogen beantworten.</h3>
-                <Button variant="outlined" style={{ marginLeft: "auto", marginRight: "auto", width: "auto"}}>
-                    <a href="../pdf/Fragebogen.pdf" download style={{textDecoration: "none", color: "#2F5A91" }}>Download PDF</a>
-                </Button>
+            <div className="ideaContainer">
+                <ThemeProvider theme={theme}>
+                    <div className="shareIdeaImg" style={{ backgroundImage: "url(../fragebogen.jpg)" }}>
+                        <h1>
+                            Teilen Sie Ihre Idee!
+                        </h1>
+                    </div>
+                    <h2 style={{ width: "40%", marginLeft: "auto", marginRight: "auto" }}>
+                        Sie haben eine Idee oder Vorschlag für kostengünstiges und energieeffizientes Bauen?
+                        eilen Sie es mit uns, indem Sie den folgenden Fragebogen beantworten.
+                    </h2>
+                    <Button variant="contained"
+                        color="primary"
+                        style={{ borderRadius: "1px", color: "white" }}
+                    >
+                        <a href="../pdf/Fragebogen.pdf" download style={{ textDecoration: "none", color: "white" }}>Download Fragebogen</a>
+                    </Button>
 
-                <h4>Kontakt:</h4>
-                <p>Fachliches Projektmanagement, Internet-Redaktion, Praxisbeispiele:</p>
-                <p style={{fontWeight: "bold"}}>Bayerisches Landesamt für Umwel</p>
-                <p>Servicestelle Ökoenergien: Telefon: (0821) 9071-5444</p>
-                <h1>
-                    <a href="https://formulare.energieatlas.bayern.de/eab/kontakt.html" style={{textDecoration: "none", color: "#2F5A91", width: "200px"}}>
-                        LINK
-                    </a>
-                </h1>
+                    <h3>Kontakt:</h3>
+                    <p>Fachliches Projektmanagement, Internet-Redaktion, Praxisbeispiele:</p>
+                    <h4>Bayerisches Landesamt für Umwel</h4>
+                    <p>Servicestelle Ökoenergien: Telefon: (0821) 9071-5444</p>
+                    <Button variant="contained"
+                        style={{ borderRadius: "1px", color: "#2f5a91" }}>
+                        <a href="https://formulare.energieatlas.bayern.de/eab/kontakt.html" style={{ textDecoration: "none", color: "white", width: "200px" }}>
+                            Kontaktformular
+                        </a>
+                    </Button>
+                </ThemeProvider>
             </div>
+
         </div>
     )
 }
